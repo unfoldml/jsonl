1494d7527d1062ad37dd92467a0e1898c7e3eed5
Marco Zocca <marco.zocca@unfoldml.com>
2022-05-03 10:03:47 +0200

    adding tests

--- jsonl-conduit/src/JSONL/Conduit.hs
+++ jsonl-conduit/src/JSONL/Conduit.hs
@@ -1,26 +1,34 @@
+{-# options_ghc -Wno-unused-imports #-}
 module JSONL.Conduit (
   -- * Encode
   jsonToLBSC
-  , sinkFile
+  -- ** I/O
+  , sinkFileC
   -- * Decode
   , jsonFromLBSC
+  -- ** I/O
+  , sourceFileC
   ) where
 
 import Data.Void (Void)
 
-import qualified Conduit as C (ConduitT, runConduit, sourceFile, sinkFile, yield, mapC, unfoldC, foldMapC)
-import Conduit ( (.|) , MonadResource)
+
 
   -- aeson
 import Data.Aeson (ToJSON(..), FromJSON(..), eitherDecode' )
 -- bytestring
-import qualified Data.ByteString.Builder as BBS (toLazyByteString, Builder, lazyByteString, string7)
-
+import qualified Data.ByteString as BS (ByteString)
+import qualified Data.ByteString.Builder as BBS (toLazyByteString, Builder)
 import qualified Data.ByteString.Internal as BS (c2w)
-import qualified Data.ByteString.Lazy as LBS (ByteString, span, toStrict)
+import qualified Data.ByteString.Char8 as BS8 (span)
+import qualified Data.ByteString.Lazy as LBS (ByteString, span, toStrict, fromStrict)
+-- conduit
+import qualified Conduit as C (ConduitT, runConduit, sourceFile, sinkFile, await, yield, mapC, unfoldC, foldMapC, foldlC)
+import Conduit ( (.|) , MonadResource)
 -- jsonl
 import JSONL (jsonLine)
 
+-- | Render a stream of JSON-encodable objects into a `LBS.ByteString`
 jsonToLBSC :: (ToJSON a, Monad m) => C.ConduitT a o m LBS.ByteString
 jsonToLBSC = BBS.toLazyByteString <$> jsonToBuilderC
 
@@ -29,16 +37,40 @@ jsonToBuilderC :: (ToJSON a, Monad m) => C.ConduitT a o m BBS.Builder
 jsonToBuilderC = C.foldMapC jsonLine
 
 -- | Render a stream of JSON-encodable objects into a JSONL file
-sinkFile :: (ToJSON a, MonadResource m) =>
-            FilePath -- ^ path of JSONL file to be created
-         -> C.ConduitT a () m ()
-sinkFile fpath = C.mapC (LBS.toStrict . BBS.toLazyByteString . jsonLine) .|
-                 C.sinkFile fpath 
+sinkFileC :: (ToJSON a, MonadResource m) =>
+             FilePath -- ^ path of JSONL file to be created
+          -> C.ConduitT a () m ()
+sinkFileC fpath = C.mapC (LBS.toStrict . BBS.toLazyByteString . jsonLine) .|
+                  C.sinkFile fpath
 
--- sourceFile fpath = C.sourceFile fpath .|
---                    go
+-- | Read a JSONL file and stream the decoded records
+sourceFileC :: (MonadResource m, FromJSON a) =>
+               FilePath -- ^ path of JSONL file to be read
+            -> C.ConduitT Void a m ()
+sourceFileC fpath = C.sourceFile fpath .|
+                    parseChunk
+
+parseChunk :: (Monad m, FromJSON a) => C.ConduitT BS.ByteString a m ()
+parseChunk = go mempty
+  where
+    go acc = do
+      mc <- C.await
+      case mc of
+        Nothing -> pure ()
+        Just x -> do
+          let
+            acc' = x <> acc
+            (s, srest) = BS8.span (== '\n') acc'
+          case eitherDecode' $ LBS.fromStrict s of
+            Left _ -> pure ()
+            Right y -> do
+              C.yield y
+              go srest
+
+
+-- parseChunk = C.foldlC mk mempty
 --   where
---     go = 
+--     mk acc x = undefined
 
 -- | Source a `LBS.ByteString` for JSONL records
 jsonFromLBSC :: (FromJSON a, Monad m) => LBS.ByteString -> C.ConduitT Void a m ()
---
+++ jsonl-conduit/test/JSONL/ConduitSpec.hs
@@ -0,0 +1,42 @@
+{-# language DeriveGeneric #-}
+{-# language OverloadedStrings #-}
+{-# options_ghc -Wno-unused-imports #-}
+module JSONL.ConduitSpec where
+
+import Data.Functor.Identity (Identity(..))
+import GHC.Generics (Generic)
+
+-- aeson
+import qualified Data.Aeson as A (FromJSON(..), ToJSON(..))
+-- bytestring
+import qualified Data.ByteString as BS (ByteString)
+import qualified Data.ByteString.Builder as BBS (toLazyByteString, Builder)
+import qualified Data.ByteString.Internal as BS (c2w)
+import qualified Data.ByteString.Char8 as BS8 (span)
+import qualified Data.ByteString.Lazy as LBS (ByteString, span, toStrict, fromStrict)
+-- conduit
+import qualified Conduit as C (ConduitT, runConduit, sourceFile, sinkFile, await, yield, yieldMany, mapC, unfoldC, foldMapC, foldlC)
+import Conduit ( (.|) , MonadResource)
+-- hspec
+import Test.Hspec (Spec, describe, it, shouldBe)
+
+import JSONL.Conduit (jsonToLBSC, jsonFromLBSC)
+
+spec :: Spec
+spec =
+  describe "JSONL.Conduit" $ do
+    it "encodes a list of JSONL records" $ do
+      datsC `shouldBe` datsLBS
+
+data Record = R String String Int Bool deriving (Eq, Generic)
+instance A.ToJSON Record
+instance A.FromJSON Record
+
+datsC :: LBS.ByteString
+datsC = runIdentity $ C.runConduit $ C.yieldMany dats .| jsonToLBSC
+
+dats :: [Record]
+dats = [R "Gilbert" "2013" 24 True,R "Alexa" "2013" 29 True, R "May" "2012B" 14 False, R "Deloise" "2012A" 19 True]
+
+datsLBS :: LBS.ByteString
+datsLBS = "[\"Gilbert\",\"2013\",24,true]\n[\"Alexa\",\"2013\",29,true]\n[\"May\",\"2012B\",14,false]\n[\"Deloise\",\"2012A\",19,true]\n"
